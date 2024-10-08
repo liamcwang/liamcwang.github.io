7ef84d40950c30901021c75fc5a8d765b51ea616
liamwang <liamcwang@gmail.com>
2024-10-07 20:31:06 -0400

    refactored webpages
    - hopefully more sensible, also found way to find path to anything
    - also hopefully this works

---
+++ src/scripts/heading.js
@@ -0,0 +1,21 @@
+function loadHeader() {
+    let port = ""
+    if(window.location.port != null)
+    {
+        port = `:${window.location.port}` 
+    }
+    const rootURL = `${window.location.protocol}//${window.location.hostname}${port}`;
+    fetch(`${rootURL}/src/fragments/header.html`, {
+        method: 'GET',
+        credentials: 'include'
+    })
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+            document.getElementById('home-link').setAttribute('href', `${rootURL}/src/pages/index.html`)
+        });
+}
+
+window.onload = function () {
+    loadHeader();    
+};
